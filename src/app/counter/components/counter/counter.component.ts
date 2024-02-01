8d09f4724d7ca5ce923d738e91d62821eb7512fa
kouski <72550619+kouski@users.noreply.github.com>
2024-02-01 20:17:39 +0100

    Cambio botones a boostrap y cambio titulo

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -5,9 +5,9 @@ import { Component } from "@angular/core";
   selector: 'app-counter',
   template: `<h3>Counter: {{ counter }}</h3>
 
-  <button (click)="incremento(8)">Incrementar counter</button>
-  <button (click)="reset()">Reset</button>
-  <button (click)="decremento()">Decrementar counter</button>`,
+  <button [class]="'btn btn-outline-success'" (click)="incremento(8)">Incrementar counter</button>
+  <button [class]="'btn btn-outline-secondary'" (click)="reset()">Reset</button>
+  <button [class]="'btn btn-outline-danger'" (click)="decremento()">Decrementar counter</button>`,
 
 })
 
